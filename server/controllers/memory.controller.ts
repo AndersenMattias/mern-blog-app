8be9fa1af3831357363ac47666ead0b2c10b8848
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-08 11:34:52 +0200

    add controller for GET all memories

--- server/controllers/memory.controller.ts
+++ server/controllers/memory.controller.ts
@@ -4,7 +4,7 @@ import path from "path";
 import fs from 'fs-extra'
 
 export  const createMemory = async (req: Request, res: Response ): Promise<Response> => {
-    const {title, image, description, year, location} = req.body
+    const {title, description, year, location} = req.body
 
     try {
         const newMemory = {title, image: req?.file?.path, description, year, location};
@@ -19,13 +19,24 @@ export  const createMemory = async (req: Request, res: Response ): Promise<Respo
 
 }
 
-export async function getMemory (req: Request, res: Response) : Promise<Response>{
+export const getMemory = async (req: Request, res: Response) : Promise<Response> => {
 
     try {
         const memory = await Memory.findById(req.params.id).lean();    
         return res.status(200).send({message: 'Memory found!', memory})
     } catch (e) {
       return  res.status(500).send({message: 'Something went wrong, try again!', e})
+    }
+
+}
+
+export  const getMemories = async (req: Request, res: Response) : Promise<Response> => {
+
+    try {
+        const memories = await Memory.find().lean()
+        return res.status(200).send({message: 'Memories found!', memories})
+    } catch (e) {
+        return res.status(500).send({message: `Couldn't find any memories :(`, e})
     }
 
 }
--- server/routes/memory.routes.ts
+++ server/routes/memory.routes.ts
@@ -1,11 +1,13 @@
 import {   Router } from "express";
-import { createMemory, getMemory } from "../controllers/memory.controller";
+import { createMemory, getMemories, getMemory } from "../controllers/memory.controller";
 import multer from "../libs/multer";
 
 
 const router = Router()
 
-router.route('/memories').post(multer.single('image'), createMemory)
+router.route('/memories')
+.get(getMemories)
+.post(multer.single('image'), createMemory)
 
 router.route('/memories/:id')
 .get(getMemory)
