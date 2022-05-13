201b6d46d1c0f73f8e8cf2728f698e8e70df09f4
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-13 15:29:50 +0200

    add locationDetails

--- src/components/LocationDetails/LocationDetails.tsx
+++ src/components/LocationDetails/LocationDetails.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import BootstrapBtn from 'components/BootstrapBtn/BootstrapBtn';
+import { ILocation, Props } from 'interfaces/interfaces';
 import { Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
-const LocationDetails = () => {
+const LocationDetails = ({ fetchData }: Props) => {
+  console.log(fetchData);
+  const { id } = useParams();
+  let location = fetchData.find((x) => x._id == id);
+
   return (
     <Container>
-      <h4>Detaljer om..</h4>
+      <h4>{location?.locationName}</h4>
+      <img src={location?.image} alt='location-img' />
+      <p>{location?.country}</p>
+      <p>{location?.city}</p>
+      <p>{location?.description}</p>
+      <p>Visited: {location?.date.toString().split('T')[0]}</p>
+      <p>{location?.createdBy}</p>
+      <p>{location?.rating}</p>
     </Container>
   );
 };
