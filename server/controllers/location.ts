0262e1574418bd6982f32138a65ddf84aba2a488
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-13 14:07:19 +0200

    fix location routes

--- server/controllers/location.ts
+++ server/controllers/location.ts
@@ -1,59 +1,95 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Location from '../models/Location';
+import { createError } from '../utils/error';
 
+// Create a location
 export const createLocation = async (
-  req: Request | any,
-  res: Response
-): Promise<Response> => {
-  const { locationName, description, date, createdBy, rating } = req.body;
-  const uploadedImg = req.file.location;
-  console.log(uploadedImg);
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const { locationName, country, city, description, date, createdBy, rating } =
+    req.body;
 
-  const location = new Location({
+  const newLocation = new Location({
     locationName,
-    imageUrl: uploadedImg,
+    country,
+    city,
     description,
     date,
+    image: req.file.location,
     createdBy,
     rating,
   });
 
   try {
-    console.log(location);
-    await location.save();
-
-    return res.status(201).send({ message: 'Location created!', location });
-  } catch (e) {
-    return res
-      .status(500)
-      .send({ message: 'Something went wrong, try again!', e });
+    const savedLocation = await newLocation.save();
+    res
+      .status(201)
+      .send({ message: 'Location has been created!', savedLocation });
+  } catch (err) {
+    next(createError(400, `Couldn't create location, try again.`));
   }
 };
 
+// update a location
+export const updateLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const updatedLocation = await Location.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res
+      .status(200)
+      .send({ message: 'Location has been updated', updatedLocation });
+  } catch (err) {
+    next(createError(400, 'Invalid updates!'));
+  }
+};
+
+// delete a location
+export const deleteLocation = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await Location.findByIdAndDelete(req.params.id);
+    res.status(200).send({ message: 'Location has been deleted.' });
+  } catch (err) {
+    next(createError(500, 'Something went wrong, try again'));
+  }
+};
+
+// get one location
 export const getLocation = async (
   req: Request,
-  res: Response
-): Promise<Response> => {
+  res: Response,
+  next: NextFunction
+) => {
   try {
-    const location = await Location.findById(req.params.id).lean();
-    return res.status(200).send({ message: 'Location found!', location });
-  } catch (e) {
-    return res
-      .status(500)
-      .send({ message: 'Something went wrong, try again!', e });
+    const location = await Location.findById(req.params.id);
+    res.status(200).send({ message: 'Location found', location });
+  } catch (err) {
+    next(createError(500, 'Something went wrong, try again'));
   }
 };
 
+// get all locations
 export const getLocations = async (
   req: Request,
-  res: Response
-): Promise<Response> => {
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const locations = await Location.find().lean();
     return res.status(200).send({ message: 'Locations found!', locations });
-  } catch (e) {
-    return res
-      .status(500)
-      .send({ message: `Couldn't find any locations :(`, e });
+  } catch (err) {
+    next(createError(500, 'Something went wrong, try again'));
   }
 };
