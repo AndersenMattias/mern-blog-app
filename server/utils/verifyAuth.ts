a00e62d558658fd6b389234264053febb15f7a0d
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-13 09:33:04 +0200

    add verification for user and error handling

---
+++ server/utils/verifyAuth.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import config from '../config/config';
+import { createError } from '../utils/error';
+
+export const verifyToken = (req: any, res: any, next: any) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, 'You are not authenticated!'));
+  }
+
+  jwt.verify(token, config.JWT, (err: any, user: any) => {
+    if (err) return next(createError(403, 'Token is not valid!'));
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (req: any, res: any, next: any) => {
+  verifyToken(req, res, (next: any) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, 'You are not authorized!'));
+    }
+  });
+};
